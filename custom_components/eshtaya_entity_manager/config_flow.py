ca0ab41c56165ec21601a8395e3c0f8d6c7eda43
badereshtaya <badereshtaya95@gmail.com>
2026-08-23 12:24:48 +0300

    Add config flow

---
+++ custom_components/eshtaya_entity_manager/config_flow.py
@@ -0,0 +1,29 @@
+"""Config flow for Eshtaya Entity Manager."""
+
+from __future__ import annotations
+
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.config_entries import ConfigFlowResult
+
+from .const import DOMAIN, NAME
+
+
+class EntityManagerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Eshtaya Entity Manager."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured()
+
+        if user_input is not None:
+            return self.async_create_entry(title=NAME, data={})
+
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
